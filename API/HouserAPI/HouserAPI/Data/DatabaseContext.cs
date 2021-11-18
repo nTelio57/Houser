1294710aba871051f476907654117bb9e8b60992
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-18 22:05:09 +0200

    Create context class and migration

---
+++ API/HouserAPI/HouserAPI/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouserAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouserAPI.Data
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> opt) : base(opt)
+        {
+            
+        }
+    }
+}
