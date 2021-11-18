47a258a7e1fe30452df584c58040e858774e1e2a
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-18 22:05:34 +0200

    Add user, roles seeding

---
+++ API/HouserAPI/HouserAPI/Data/Seed/RoleSeed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouserAPI.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace HouserAPI.Data.Seed
+{
+    public static class RoleSeed
+    {
+        public static void Seed(RoleManager<IdentityRole> roleManager)
+        {
+            foreach (var role in UserRoles.All)
+            {
+                var roleExist = roleManager.RoleExistsAsync(role).Result;
+                if (!roleExist)
+                {
+                    roleManager.CreateAsync(new IdentityRole(role)).Wait();
+                }
+            }
+        }
+    }
+}
