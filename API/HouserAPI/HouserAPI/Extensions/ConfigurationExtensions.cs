eccf3317772d72347f47a469112b67a033ea3a68
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-18 22:06:29 +0200

    Update startup class

---
+++ API/HouserAPI/HouserAPI/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HouserAPI.Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HouserAPI.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection AddAuthorizationDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddTransient<ITokenManager, TokenManager>();
+        }
+
+        public static AuthenticationBuilder AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters.ValidAudience = configuration["JwtSettings:ValidAudience"];
+                    options.TokenValidationParameters.ValidIssuer = configuration["JwtSettings:ValidIssuer"];
+                    options.TokenValidationParameters.IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Secret"]));
+                });
+        }
+    }
+}
--- API/HouserAPI/HouserAPI/Startup.cs
+++ API/HouserAPI/HouserAPI/Startup.cs
@@ -11,6 +11,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HouserAPI.Data;
+using HouserAPI.Data.Seed;
+using HouserAPI.Extensions;
+using HouserAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Serialization;
 
 namespace HouserAPI
 {
@@ -25,20 +33,63 @@ namespace HouserAPI
         
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HouserAPI", Version = "v1" });
             });
+
+            var builder = new MySqlConnectionStringBuilder(Configuration.GetConnectionString("HouserConnection"))
+            {
+                Server = Configuration["Server"],
+                Database = Configuration["Database"],
+                UserID = Configuration["User"],
+                Password = Configuration["Password"]
+            };
+            services.AddDbContext<DatabaseContext>(opt =>
+                opt.UseMySQL(builder.ConnectionString));
+
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddAuthorizationDependencies(Configuration);
+            services.AddJwtAuthentication(Configuration);
         }
         
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DatabaseContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HouserAPI v1"));
+
+                var seed = new SeedData(context, userManager, roleManager);
+                seed.Seed();
             }
 
             app.UseHttpsRedirection();
@@ -46,6 +97,7 @@ namespace HouserAPI
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
