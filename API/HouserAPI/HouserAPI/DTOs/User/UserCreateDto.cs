17339de1a240b1297ed2d3fb087bdf2886e4928b
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-18 22:03:39 +0200

    Create User entity

---
+++ API/HouserAPI/HouserAPI/DTOs/User/UserCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouserAPI.DTOs.User
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string City{ get; set; }
+    }
+}
---
+++ API/HouserAPI/HouserAPI/DTOs/User/UserReadDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouserAPI.DTOs.User
+{
+    public class UserReadDto
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
