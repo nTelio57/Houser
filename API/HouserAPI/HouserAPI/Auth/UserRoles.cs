605570cd895c9adf8503787dd9af3ddc247f1e27
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-18 22:06:17 +0200

    Add auth utility classes

---
+++ API/HouserAPI/HouserAPI/Auth/UserRoles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HouserAPI.Auth
+{
+    public class RolesAttribute : AuthorizeAttribute
+    {
+        public RolesAttribute(params string[] roles)
+        {
+            Roles = string.Join(",", roles);
+        }
+    }
+
+    public static class UserRoles
+    {
+        public const string Admin = nameof(Admin);
+        public const string Basic = nameof(Basic);
+
+        public static readonly IReadOnlyCollection<string> All = new[] { Admin, Basic };
+    }
+}
---
+++ API/HouserAPI/HouserAPI/Utilities/Cryptography.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HouserAPI.Utilities
+{
+    public class Cryptography
+    {
+        public static string GenerateSalt(int saltLength = 32)
+        {
+            byte[] salt = new byte[saltLength];
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
+        }
+    }
+}
