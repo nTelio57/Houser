c40c23a5f539f9039c58a97de3836b2f2a7982ee
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-20 21:54:27 +0200

    Create image service classes

---
+++ API/HouserAPI/HouserAPI/Services/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HouserAPI.DTOs.Image;
+using Microsoft.AspNetCore.Http;
+
+namespace HouserAPI.Services
+{
+    public interface IImageService
+    {
+        Task<ImageReadDto> Create(string userId, IFormFile image);
+        Task<IEnumerable<ImageReadDto>> GetAll();
+        Task<ImageReadDto> GetById(int id);
+        Task<IEnumerable<ImageReadDto>> GetAllByUser(string id);
+        Task<bool> Delete(int id);
+    }
+}
