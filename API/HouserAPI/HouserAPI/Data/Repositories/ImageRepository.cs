4764aa87e09b284a8926a4933017ce0a4b094141
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-20 21:49:49 +0200

    Create Image repository

---
+++ API/HouserAPI/HouserAPI/Data/Repositories/ImageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouserAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouserAPI.Data.Repositories
+{
+    public class ImageRepository : Repository<Image>
+    {
+        protected override DbSet<Image> Entities { get; }
+        public ImageRepository(DatabaseContext context) : base(context)
+        {
+            Entities = context.Images;
+        }
+
+        public async Task<IEnumerable<Image>> GetAllByUser(string userId)
+        {
+            return await IncludeDependencies(Entities).Where(x => x.UserId == userId).ToListAsync();
+        }
+    }
+}
