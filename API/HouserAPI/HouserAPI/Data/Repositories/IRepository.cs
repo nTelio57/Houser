1c72312f1e73b8eb4f52999f6d09932433706712
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-20 21:48:53 +0200

    Create base repository class

---
+++ API/HouserAPI/HouserAPI/Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HouserAPI.Data.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        Task<bool> SaveChanges();
+        Task Create(TEntity entity);
+        Task<TEntity> GetById(int id);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task Delete(TEntity entity);
+    }
+}
