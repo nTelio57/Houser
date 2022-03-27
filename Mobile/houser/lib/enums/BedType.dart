26dcbebc9389e48c2a8e951d4afb8c237014405f
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-03-27 19:49:18 +0300

    Update placeholder offer class

---
+++ Mobile/houser/lib/enums/BedType.dart
@@ -0,0 +1,6 @@
+enum BedType{
+  noBed,
+  single,
+  double,
+  sofa,
+}
--- Mobile/houser/lib/models/Offer.dart
+++ Mobile/houser/lib/models/Offer.dart
@@ -1,13 +1,31 @@
+import 'package:houser/enums/BedType.dart';
+
 class Offer{
   bool isActive = true;
   bool isVisible = true;
+  DateTime uploadDate = DateTime.now();
 
   String title = 'Ieškomas vienas kambariokas dviem mėnesiams';
-  DateTime uploadDate = DateTime.now();
-  String address = 'Studentų g. 50 ';
   String city = 'Kaunas';
+  String address = 'Studentų g. 50 ';
+
+  double monthlyPrice  = 100;
+  bool utilityBillsRequired = true;
+
+  DateTime availableFrom = DateTime.now();
+  DateTime availableTo = DateTime.now().add(const Duration(days: 90));
   int freeRoomCount = 1;
   int totalRoomCount = 3;
+
+  bool ruleSmoking = true;
+  bool ruleAnimals = true;
+
+  int bedCount = 1;
+  BedType bedType = BedType.single;
+
+  bool accommodationTV = true;
+  bool accommodationWIFI = true;
+  bool accommodationAC = true;
 
   Offer({required this.title, required this.uploadDate, required this.address, required this.city, required this.freeRoomCount, required this.totalRoomCount, required this.isActive, required this.isVisible});
 
