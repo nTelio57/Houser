11982ea9e2d8d31abc6aca2ba2b48cd00f3c1ac5
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-22 01:57:21 +0300

    Fix port

--- Recommendation/Recommendation/Recommendation/Main.py
+++ Recommendation/Recommendation/Recommendation/Main.py
@@ -2,7 +2,4 @@ from DatabaseContext import app
 from RecommendationController import *
 
 if __name__ == '__main__':
-    if app.debug:
-        app.run(port=5002)
-    else:
-        app.run()
+    app.run(port=app.port)
