926dcc99519dd60af07991e7029095d82ce2ce20
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-21 21:32:42 +0300

    Add environment variables

--- Recommendation/Recommendation/Recommendation/DatabaseContext.py
+++ Recommendation/Recommendation/Recommendation/DatabaseContext.py
@@ -1,8 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
+import os
+from os import environ as env
+
+connectionString = "mysql+mysqlconnector://root:@localhost/Houser"
+debugMode = True
 
 app = Flask(__name__)
-app.debug = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:@localhost/Houser"
+
+if 'HOUSER_DB' in env:
+    debugMode = False
+    connectionString = env['HOUSER_DB']
+
+app.debug = debugMode
+app.config["SQLALCHEMY_DATABASE_URI"] = connectionString
 
 db = SQLAlchemy(app)
