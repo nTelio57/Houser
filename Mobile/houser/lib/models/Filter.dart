643bcbbbe092a8d15294560d1aff02213b6a2c5d
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-24 14:54:28 +0300

    Add equal check for filter classes

--- Mobile/houser/lib/models/Filter.dart
+++ Mobile/houser/lib/models/Filter.dart
@@ -1,9 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:houser/enums/FilterType.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'Filter.g.dart';
 
 @JsonSerializable()
-class Filter {
+class Filter extends Equatable{
   Filter(this.id, this.userId, this.filterType);
 
   int id;
@@ -13,4 +14,7 @@ class Filter {
   factory Filter.fromJson(Map<String, dynamic> json) => _$FilterFromJson(json);
 
   Map<String, dynamic> toJson() => _$FilterToJson(this);
+
+  @override
+  List<Object?> get props => [id, userId, filterType];
 }
