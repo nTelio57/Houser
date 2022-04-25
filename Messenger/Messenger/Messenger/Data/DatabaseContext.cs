e65f0a4f10739dbf839a7c961249d2f83da3a6e9
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-25 15:09:12 +0300

    Add repositories

---
+++ Messenger/Messenger/Messenger/Data/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Messenger.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messenger.Data
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Message> Messages{ get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> opt) : base(opt)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Match>().ToTable(nameof(Matches), t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<User>().ToTable("AspNetUsers", t => t.ExcludeFromMigrations());
+
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRoles", t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("AspNetUserClaims", t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("AspNetUserLogins", t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("AspNetUserTokens", t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("AspNetRoleClaims", t => t.ExcludeFromMigrations());
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("AspNetUserRoles", t => t.ExcludeFromMigrations());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
