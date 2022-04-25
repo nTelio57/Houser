98ee5d3adaaf1aac6a947899a14177e3a3622f9e
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-26 00:15:39 +0300

    Update send message function

--- Messenger/Messenger/Messenger/Hubs/ChatHub.cs
+++ Messenger/Messenger/Messenger/Hubs/ChatHub.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Messenger.Auth;
 using Messenger.DTOs.Message;
 using Messenger.Models;
 using Messenger.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Messenger.Hubs
 {
+    
     public class ChatHub : Hub
     {
         private readonly IMessageService _messageService;
-        private static readonly List<string> ConnectedUsers = new();
+        private static readonly Dictionary<string, string> ConnectedUsers = new();
         private static readonly List<Match> Matches = new();
 
         public ChatHub(IMessageService messageService)
@@ -21,31 +26,51 @@ namespace Messenger.Hubs
 
         private static Match GetMatch(int matchId) =>
             Matches.FirstOrDefault(match => match.Id == matchId);
+        
         public async Task SendMessage(int matchId, string senderUserId, string message)
         {
-            MessageCreateDto newMessage = new MessageCreateDto();
-            newMessage.MatchId = matchId;
-            newMessage.SenderId = senderUserId;
-            newMessage.Content = message;
+            MessageCreateDto newMessage = new MessageCreateDto
+            {
+                MatchId = matchId, SenderId = senderUserId, Content = message
+            };
 
             await _messageService.Create(newMessage);
-
-            Match match = GetMatch(matchId);
+            
+            var match = await _messageService.GetMatchById(matchId);
             if (match == null)
                 return;
 
-            var receiver = match.GetReceiver(senderUserId);
-            if (receiver == null)
+            var receiverId = match.GetReceiverId(senderUserId);
+            if (string.IsNullOrEmpty(receiverId))
                 return;
-
-            await Clients.All.SendAsync("ReceiveMessage", senderUserId, message);
+            
+            var connectionIds = ConnectedUsers.Where(x => x.Value == receiverId).Select(x => x.Key).ToList();
+            foreach (string connectionId in connectionIds)
+            {
+                await Clients.Client(connectionId).SendAsync("ReceiveMessage", matchId, senderUserId, message);
+            }
         }
-        public override async Task OnConnectedAsync()
+
+        public async Task Login(string userId)
         {
-            var name = Context.User.Identity.Name;
-            ConnectedUsers.Add(Context.ConnectionId);
-            await Groups.AddToGroupAsync(Context.ConnectionId, "ChatRoom");
-            await base.OnConnectedAsync();
+            ConnectedUsers.Add(Context.ConnectionId, userId);
+        }
+
+        public async Task Disconnect(string userId)
+        {
+            ConnectedUsers.Add(Context.ConnectionId, userId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = ConnectedUsers[Context.ConnectionId];
+            var connectionIds = ConnectedUsers.Where(x => x.Value == userId).Select(x => x.Key).ToList();
+            foreach (string connectionId in connectionIds)
+            {
+                ConnectedUsers.Remove(connectionId);
+            }
+
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
