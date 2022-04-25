05b4b8b8bb4f52a1674dcbf501b958dcfdc18e5c
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2022-04-25 15:10:11 +0300

    Add hub

--- Messenger/Messenger/Messenger/Hubs/ChatHub.cs
+++ Messenger/Messenger/Messenger/Hubs/ChatHub.cs
@@ -1,13 +1,51 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Messenger.DTOs.Message;
+using Messenger.Models;
+using Messenger.Services;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Messenger.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private readonly IMessageService _messageService;
+        private static readonly List<string> ConnectedUsers = new();
+        private static readonly List<Match> Matches = new();
+
+        public ChatHub(IMessageService messageService)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            _messageService = messageService;
+        }
+
+        private static Match GetMatch(int matchId) =>
+            Matches.FirstOrDefault(match => match.Id == matchId);
+        public async Task SendMessage(int matchId, string senderUserId, string message)
+        {
+            MessageCreateDto newMessage = new MessageCreateDto();
+            newMessage.MatchId = matchId;
+            newMessage.SenderId = senderUserId;
+            newMessage.Content = message;
+
+            await _messageService.Create(newMessage);
+
+            Match match = GetMatch(matchId);
+            if (match == null)
+                return;
+
+            var receiver = match.GetReceiver(senderUserId);
+            if (receiver == null)
+                return;
+
+            await Clients.All.SendAsync("ReceiveMessage", senderUserId, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var name = Context.User.Identity.Name;
+            ConnectedUsers.Add(Context.ConnectionId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, "ChatRoom");
+            await base.OnConnectedAsync();
         }
     }
 }
